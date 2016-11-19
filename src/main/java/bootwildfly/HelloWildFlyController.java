d132cf1892899b206b7179f672948b239c35dd36
oliver <o.guggenbuehl@gmail.com>
2016-11-19 13:14:43 +0100

    Update HelloWildFlyController.java

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -2,6 +2,7 @@ package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HelloWildFlyController {
@@ -9,7 +10,12 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("HOSTNAME")){
+            String host = env.get("HOSTNAME");
+            System.out.println(host);
+        }
         String name = "oli";
-        return ("Hello, SpringBoot on Wildfly" + name);
+        return ("Hello, SpringBoot on Wildfly" + name + ": " + host);
     }
 }
